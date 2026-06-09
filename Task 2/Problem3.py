243b088c86aa85cea7454a760b604d344d038aac
Abu Sayed <sayedmucse180@gmail.com>
2026-06-09 16:23:59 +0600

    Implement reverse transcription of DNA sequence
    Reverse transcribe a DNA sequence by replacing A with T, T with A, C with G, and G with C.

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,14 @@
+str=input()
 
+lst=''
+for i in range(len(str)-1,-1,-1):
+    if str[i]=='A':
+        lst+='T'
+    elif str[i]=='T':
+        lst+='A'
+    elif str[i]=='C':
+        lst+='G'
+    elif str[i]=='G':
+        lst+='C'
+
+print(lst) 
