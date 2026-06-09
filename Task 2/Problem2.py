a987be38196dcbf44a88ca72e62bd961faaed0e1
Abu Sayed <sayedmucse180@gmail.com>
2026-06-09 16:26:34 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,15 @@
+txt=input()
+k=int(input())
 
+frq={}
+
+for i in range(len(txt)- k+1):
+    kmer=txt[i:i+k]
+    frq[kmer]=frq.get(kmer,0)+1
+  
+
+mx=max(frq.values())
+        
+for kmer in frq:
+    if frq[kmer] == mx:
+        print(kmer,end=" ")
