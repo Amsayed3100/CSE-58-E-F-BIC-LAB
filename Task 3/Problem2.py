3309e935b1d9ffb5350f901969b0fd8e2e2101ab
Abu Sayed <sayedmucse180@gmail.com>
2026-06-16 16:27:58 +0600

    Add pattern matching with mismatch tolerance
    Implement pattern matching with allowed mismatches.

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,20 @@
+pattern=input().strip()
+text=input().strip()
+d=int(input())
 
+lst=[]
+k=len(pattern)
+n=len(text)
+for i in range(n- k+1):
+    miss=0
+    
+    for j  in range(k):
+        if pattern[j] != text[i+j]:
+            miss+=1
+          
+    if miss<=d:
+        lst.append(i)
+
+
+print(*lst)    
+        
